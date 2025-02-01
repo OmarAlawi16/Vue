ed8b278ef75e4d0f2faac3d426cf27b7b56ea85f
Omar Alawi <omar.alawi17@gmail.com>
2025-02-01 10:19:23 +0100

    Added SingleTodo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import SingleTodo from '@/components/SingleTodo.vue';
+
 const todos = ref<string[]>([]);
+
 const todoName = ref('');
+
 function addTodo() {
   todos.value.push(todoName.value);
 }
@@ -10,14 +14,14 @@ function addTodo() {
 <template>
   <main>
     <div class="my-8">
-      <ul v-if="todos.length > 0">
-        <li v-for="(todo, index) in todos" :key="index">
-          {{ todo }}
-        </li>
-      </ul>
-      <p v-if="todos.length === 0">Du är klar med alla uppgifter!</p>
+      <template v-if="todos.length > 0">
+        <div v-for="(todo, index) in todos" :key="index">
+          <SingleTodo :todo-text="todo" />
+        </div>
+      </template> 
+      <p v-else>You are done with all tasks</p>
     </div>
     <input type="text" v-model="todoName" @keyup.enter="addTodo" />
-    <button @click="addTodo">Lägg till todo</button>
+    <button @click="addTodo">add a todo</button>
   </main>
 </template>
