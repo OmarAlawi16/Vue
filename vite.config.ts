89b3d39035f1e8d8d63535f6c6fb1cca82421e99
Omar Alawi <omar.alawi17@gmail.com>
2025-02-02 08:58:52 +0100

    Added gh deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/fed24d-arbetsmetodik-inl-2-vue-OmarAlawi16/',
   plugins: [
     vue(),
     vueDevTools(),
@@ -15,4 +15,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-})
+});
